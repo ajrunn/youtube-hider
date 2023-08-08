48d6d8d69909725d425c743a2c5bd41d14c3c787
ajrunn <141765380+ajrunn@users.noreply.github.com>
2023-08-08 12:45:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const query = document.querySelectorAll('div#channel-info.style-scope.ytd-video-renderer, span.inline-metadata-item.style-scope.ytd-video-meta-block');
+
+query.forEach(elem => elem.style.display = 'none');
